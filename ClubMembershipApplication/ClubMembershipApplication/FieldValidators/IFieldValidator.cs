0c95be20efd403ba85d2aac8f9a3011b17aa0ff6
JiheiBot <2757174746@qq.com>
2023-12-24 17:45:19 +0000

    Created FieldValidator API
    created APIs for futher usage. creat special validators for future views individually

---
+++ ClubMembershipApplication/ClubMembershipApplication/FieldValidators/IFieldValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication.FieldValidators
+{
+    public delegate bool FieldValidatorDel(int fieldIndex, string fieldValue, string[] fieldArray, out string fieldInvalidMessage);
+    public interface IFieldValidator
+    {
+        void InitialiseValidatorDelegates();
+        string[] FieldArray { get; }
+        FieldValidatorDel ValidatorDel { get; }
+
+    }
+}
