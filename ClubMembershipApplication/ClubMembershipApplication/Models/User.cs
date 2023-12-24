e8e387913d46c12923a2d3e4856496b7433fd161
JiheiBot <2757174746@qq.com>
2023-12-24 15:19:01 +0000

    Initial try
    Created the User data with the connection to the Sqlite database. created the database and the user table. Created Validators delegate methods for furture multiple validation. using enum to obtain readable code. created interface for different specific validation usage

---
+++ ClubMembershipApplication/ClubMembershipApplication/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        //here we use code first apporach to set Id to be the primary key and it will auto increase
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime DateOfBirth {  get; set; }
+
+        public string PhoneNumber {  get; set; }
+
+        public string AddressFirstLine { get; set; }
+
+        public string AddressLastLine { get; set;}
+
+        public string AddressCity { get; set; }
+
+        public string PostCode { get; set; }
+    }
+}
