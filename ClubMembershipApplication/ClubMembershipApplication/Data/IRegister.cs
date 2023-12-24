e771667cf26753515ed9799282a1eeb827504dea
JiheiBot <2757174746@qq.com>
2023-12-24 21:18:52 +0000

    Interfaces created
    register interface abstact the method that will validate the inputs and will initilise it inside the database. for the ILogin, it will take the email and password as parameter and return the user object.

---
+++ ClubMembershipApplication/ClubMembershipApplication/Data/IRegister.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication.Data
+{
+    public interface IRegister
+    {   
+        //the parameter represents the correct form of data which will be proceed to be stored in to the database
+        bool Register(string[] fields);
+        bool EmailExists(string emailAddress);
+    }
+}
